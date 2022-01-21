6fa9c1b81bcacdd064cf8b9d4e7930097761d0dc
Max Kydanchuk <maxym.kydanchuk@gmail.com>
2022-01-21 16:25:19 +0200

    fix login test

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import {render} from './utils/test-utils';
 import App from "./components/app/app";
+import apiService from "./services/api-service";
+
+const api = new apiService()
 
 let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWNlZmZkZDgyOWI1M2RiMDg4MTcwZjciLCJpYXQiOjE2NDI1MDA3MDIsImV4cCI6MTY0MjUwNDMwMn0.j7PYFCQPqK2Nt0d0zcjTwAlAEICT3yrV68ujCYC1UgI;'
 
-const formData = {
-    email: 'email@test.com',
-    password: 'testpassword'
-};
-
-const responseData = {
+const mockedData = {
     token: token,
     email: 'email@test.com',
 }
 
-const apiLoginRequest = (url, data) => {
-    return new Promise((resolve) => {
-        resolve({token: token, email: data.email});
-    })
-};
-
-const testLogin = (data) => {
-    return apiLoginRequest('/api/login', data).then((data) => data)
-}
-
 test('App has body', () => {
+    jest.useFakeTimers('legacy');
     render(<App/>);
     const body = document.querySelector('body');
     expect(body).toBeInTheDocument();
 });
 
-test('Login', () => {
-    expect.assertions(1);
-    return testLogin(formData).then((data) => expect(data).toEqual(responseData))
+test('Login',   async() => {
+    const loginMock = jest.spyOn(api, 'getUser');
+    loginMock.mockResolvedValue(mockedData)
+
+    const response = await api.getUser();
+
+    expect(response).toEqual(mockedData)
 })
