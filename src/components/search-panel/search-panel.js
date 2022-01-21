cb8ac98a079962b3d747d867ac6bcb2fbd90d8cc
Max Kydanchuk <maxym.kydanchuk@gmail.com>
2022-01-21 09:32:12 +0200

    ui fixes, refactored grid components, delete redundant components

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,27 +1,31 @@
 import React from "react";
-import { Input, Flex } from "@chakra-ui/react";
+import {Input, Flex} from "@chakra-ui/react";
 
-const SearchPanel = ({ onSearchChange, inputValue }) => {
-  return (
-<>
-    <Flex
-      className="table__header_search"
-      align="center"
-      justify="center"
-      p={(2, 3)}
-    >
-      <Input
-        type="search"
-        placeholder="Search"
-        size="md"
-        variant="filled"
-        maxW="50%"
-        value={inputValue}
-        onChange={onSearchChange}
-      />
-    </Flex>
-</>
-  );
+const SearchPanel = ({onSearchChange, inputValue}) => {
+    return (
+        <>
+            <Flex
+                className="table__header_search"
+                align="center"
+                justify="center"
+                p={[2, 3]}
+
+            >
+                <Input
+                    type="search"
+                    placeholder="Search"
+                    size="md"
+                    variant="filled"
+                    maxW="50%"
+                    _focus={{
+                        color: "#FFFFF"
+                    }}
+                    value={inputValue}
+                    onChange={onSearchChange}
+                />
+            </Flex>
+        </>
+    );
 };
 
 export default SearchPanel;
